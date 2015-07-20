63c7b82fdd19ffcb13c970148ed536fb5e51f904
Brett Casbeer <casbeebc@gmail.com>
2015-07-19 20:04:37 -0700

    Separated the MetalViewControllerDelegate from the MetalViewController class

---
+++ DataGridView/MetalViewControllerDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  MetalViewControllerDelegate.swift
+//  DataGridView
+//
+//  Created by brett on 7/19/15.
+//  Copyright (c) 2015 DataMingle. All rights reserved.
+//
+
+import UIKit
+import Metal
+import QuartzCore
+
+protocol MetalViewControllerDelegate : class{
+    func updateLogic(timeSinceLastUpdate:CFTimeInterval)
+    func renderObjects(drawable:CAMetalDrawable)
+}
